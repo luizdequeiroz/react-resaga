363bd188e4f1fcc219c10d31de4d6298b6b4f970
Luiz Viana de Queiroz Neto <luizdequeiroz@outlook.com.br>
2020-01-31 16:15:56 -0300

    adding more exportations (set and dispatch)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider, useSelector, useDispatch } from 'react-redux';
-import { reducer as reducerForms } from 'redux-form';
 import createSagaMiddleware from 'redux-saga';
 import { all, takeEvery, put as _put } from 'redux-saga/effects';
 
@@ -19,31 +18,22 @@ const genericReducer = (state = {}, action) => {
 
 const combination = () => ({ reducers: genericReducer });
 const devToolsConfiguration = () => window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const reduxFormConfiguration = () => combineReducers({ ...combination(), reducerForms });
 const sagaConfiguration = () => applyMiddleware(_sagaMiddleware = createSagaMiddleware())(createStore);
 
 const store = () => createStore(combineReducers(combination()));
 const storeDevTools = () => createStore(combineReducers(combination()), devToolsConfiguration());
-const storeReduxForm = () => createStore(reduxFormConfiguration());
-const storeReduxFormDevTools = () => createStore(reduxFormConfiguration(), devToolsConfiguration());
 const storeSaga = () => sagaConfiguration()(combineReducers(combination()));
 const storeSagaDevTools = () => sagaConfiguration()(combineReducers(combination()), devToolsConfiguration());
-const storeReduxFormSaga = () => sagaConfiguration()(reduxFormConfiguration());
-const storeReduxFormSagaDevTools = () => sagaConfiguration()(reduxFormConfiguration(), devToolsConfiguration());
 
 const _dispatchers = {};
 
-export const Container = ({ children, reduxForm, sagas, devtools }) => {
+export const Container = ({ children, sagas, devtools }) => {
 
     const provider = React.createElement(Provider, {
-        store: reduxForm && sagas && devtools ? storeReduxFormSagaDevTools()
-            : reduxForm && sagas ? storeReduxFormSaga()
-                : sagas && devtools ? storeSagaDevTools()
-                    : sagas ? storeSaga()
-                        : reduxForm && devtools ? storeReduxFormDevTools()
-                            : reduxForm ? storeReduxForm()
-                                : devtools ? storeDevTools()
-                                    : store
+        store: sagas && devtools ? storeSagaDevTools() :
+            sagas ? storeSaga() :
+                devtools ? storeDevTools() :
+                    store
     }, children);
 
     const watchers = [];
@@ -65,7 +55,7 @@ export const Container = ({ children, reduxForm, sagas, devtools }) => {
     _sagaMiddleware.run(root);
 
     return provider;
-}
+};
 
 export const useReducers = (...keys) => {
     const state = useSelector(state => {
@@ -79,7 +69,7 @@ export const useReducers = (...keys) => {
     });
 
     const dispatch = useDispatch();
-    state.set = (key, value) => dispatch({ type: 'set', key, value });
+    state.set = (key, value) => dispatch(set(key, value));
     Object.keys(_dispatchers).forEach(key => {
         state[key] = (payload) => {
             dispatch({ ..._dispatchers[key], payload });
@@ -87,13 +77,16 @@ export const useReducers = (...keys) => {
     });
 
     return state;
-}
+};
 
 export const sagaMiddleware = _sagaMiddleware;
-export const put = _put;
+export const set = (key, value) => ({ type: 'set', key, value });
+export const dispatch = _put;
 
 export default {
     Container,
     useReducers,
-    sagaMiddleware: _sagaMiddleware
+    sagaMiddleware: _sagaMiddleware,
+    set,
+    dispatch: _put
 };
